9309c25e0dc98f7e2bb07e0fe59270370a1550a7
Ashraf-Toubar <ashraft477@gmail.com>
2022-09-07 22:14:33 +0200

    add ProductList

--- project/urls.py
+++ project/urls.py
@@ -15,7 +15,14 @@ Including another URLconf
 """
 from django.contrib import admin
 from django.urls import path
+from product.views import ProductList
+
 
 urlpatterns = [
     path('admin/', admin.site.urls),
+    
+    path('product',ProductList.as_view())
+
 ]
+#as_view()---> function يتخليه كأنه 
+
